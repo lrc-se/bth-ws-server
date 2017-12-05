d0b9096af5e9f23f2532b1a8c9bf1aec882fb18e
Kalle Brisland <kabc16@student.bth.se>
2017-12-05 20:30:57 +0100

    Added test case for connection-level handlers

--- test/ws-server.js
+++ test/ws-server.js
@@ -7,14 +7,16 @@
 const tap = require("tap");
 const wsServer = require("../index");
 const http = require("http");
+const WebSocket = require("ws");
 
 
 let httpServer;
+let port = process.env.WS_PORT || 1701;
 
 
 tap.beforeEach(function(done) {
     httpServer = http.createServer();
-    httpServer.listen(process.env.WS_PORT || 1701, done);
+    httpServer.listen(port, done);
 });
 
 
@@ -35,4 +37,32 @@ tap.test("Test independence of constructed objects", function(t) {
         "underlying WebSockets server not shared between objects"
     );
     t.end();
+});
+
+
+tap.test("Test connection-level handlers", function(t) {
+    t.plan(5);
+    
+    let server = wsServer({ server: httpServer }, {
+        protocolHandler: function(protocols) {
+            return protocols[0];
+        },
+        
+        connectionHandler: function(client) {
+            t.type(client.socket, WebSocket, "client object contains WebSocket object");
+            t.type(client.request, http.IncomingMessage, "client object contains request object");
+            
+            client.socket.close(4000, "Test reason");
+        },
+        
+        closeHandler: function(code, reason) {
+            t.equal(code, 4000, "correct closing code received");
+            t.equal(reason, "Test reason", "correct closing reason received");
+        }
+    });
+    
+    let socket = new WebSocket("ws://localhost:" + port, "test-protocol");
+    socket.onopen = function() {
+        t.equal(socket.protocol, "test-protocol", "correct subprotocol selected");
+    };
 });
